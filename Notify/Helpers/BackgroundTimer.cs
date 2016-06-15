5e8aa7a4f7273231ce65756aebe9982a32a8152d
Eldho Abe <eldhoabe.07@gmail.com>
2016-06-15 15:08:42 +0530

    Notification of system tray icon

---
+++ Notify/Helpers/BackgroundTimer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace Notify.Helpers
+{
+    public class BackgroundDispatcherTimer : DispatcherTimer
+    {
+        #region Constructor
+
+        public BackgroundDispatcherTimer()
+        {
+            Tick += BackgroundDispatcherTimer_Tick;
+        }
+
+        #endregion
+
+        #region Public Properties
+
+        public bool IsReentrant { get; set; }
+
+        public bool IsRunning { get; private set; }
+
+        public Func<Task> TickTask { get; set; }
+
+        #endregion
+
+        #region Event Handler
+
+        private async void BackgroundDispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            if (TickTask == null) return;
+
+            // previous task hasn't completed
+            if (IsRunning && !IsReentrant) return;
+
+            try
+            {
+                // running the background task
+                IsRunning = true;
+
+                await TickTask.Invoke();
+            }
+            catch (Exception)
+            {
+                Debug.WriteLine("Background dispatcher timer Task Failed");
+            }
+            finally
+            {
+                IsRunning = false;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ Notify/MainWindow.xaml.cs
@@ -0,0 +1,81 @@
+﻿using Hardcodet.Wpf.TaskbarNotification;
+using Notify.Helpers;
+using System;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace Notify
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        internal BackgroundDispatcherTimer backgroundTimer { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            StartTimer();
+            WindowState = System.Windows.WindowState.Minimized;
+        }
+
+        private void NumberValidationTextBox(object sender, System.Windows.Input.TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+
+        /// <summary>
+        /// On state changed
+        /// </summary>
+        /// <param name="e"></param>
+        protected override void OnStateChanged(EventArgs e)
+        {
+            if (WindowState.Minimized == System.Windows.WindowState.Minimized)
+                this.Hide();
+
+            base.OnStateChanged(e);
+        }
+
+        #region Private methods
+
+        private void StartTimer()
+        {
+            backgroundTimer = new BackgroundDispatcherTimer();
+            backgroundTimer.IsReentrant = false;
+            backgroundTimer.Interval = TimeSpan.FromSeconds(int.Parse(IntervalTextBox.Text));
+            backgroundTimer.TickTask = async () =>
+            {
+                try
+                {
+                    //await Task.Run(() => DatabaseVersionBLInstance.GetLatestDatbaseVersion());
+                    //IsConnectionLostNotified = false;
+                    ShowContent("Hi", "dfs", BalloonIcon.Info);
+                }
+                catch (Exception)
+                {
+
+
+                    throw;
+                }
+            };
+            backgroundTimer.Start();
+        }
+
+        private void ShowContent(string title, string content, BalloonIcon iconType)
+        {
+            this.WindowState = System.Windows.WindowState.Minimized;
+            this.myNotifyIcon.ShowBalloonTip(title, content, iconType);
+        }
+        
+        #endregion
+        
+
+    }
+}
